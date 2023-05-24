1f172f62cb36eec2e2677eb2e34c8318c6dde3b9
王达昌 <dc.wang@idiaoyan.com>
2023-05-24 16:39:07 +0800

    初始化

---
+++ src/test/java/com/selene/spring/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.selene.spring;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
